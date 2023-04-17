0a905f6b89ec0acb5f5079b1f51be88ae8a5c527
TOM4S <tomyamigos@gmail.com>
2023-04-17 19:01:31 -0300

    update 7
    update 7

--- src/test/java/programaDeportivo/test.java
+++ src/test/java/programaDeportivo/test.java
@@ -1,7 +1,5 @@
 package programaDeportivo;
 
-import static org.junit.Assert.*;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
@@ -11,54 +9,86 @@ public class Test {
 
 	@org.junit.Test
 	public void archivosCorrectos() {
+		System.out.println("Test OK");
 		String input = 	"D:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\main\\resources\\resultado.txt\nD:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\main\\resources\\conexionYPuntos.txt";
 		InputStream in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
 
 		Programa pr = new Programa();
 		pr.inicializar();
+		System.out.println("-------------");
 	}
 	
 	@org.junit.Test
 	public void dosArchivosVacios() {
+		System.out.println("Test archivos vacios");
 		String input = 	"D:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\vacio.txt\nD:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\vacio.txt";
 		InputStream in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
 
 		Programa pr = new Programa();
-	    try {
-	        pr.inicializar();  
-	    } catch (Exception e) {
-	        fail(e.getMessage());
-	    }
+
+	    pr.inicializar();
+	    System.out.println("-------------");
 	}
 	
 	@org.junit.Test
 	public void primerArchivoVacios() {
+		System.out.println("Test primer archivo vacio");
 		String input = 	"D:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\vacio.txt\nD:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\conexionYPuntos.txt";
 		InputStream in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
 
 		Programa pr = new Programa();
-	    try {
-	        pr.inicializar();  
-	    } catch (Exception e) {
-	        fail(e.getMessage());
-	    }
+		pr.inicializar(); 
+	    System.out.println("-------------");
 	}
 	
 	@org.junit.Test
 	public void segundoArchivoVacio() {
+		System.out.println("Test segundo archivo vacio");
 		String input = 	"D:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\resultado.txt\nD:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\vacio.txt";
 		InputStream in = new ByteArrayInputStream(input.getBytes());
 		System.setIn(in);
 
 		Programa pr = new Programa();
-	    try {
-	        pr.inicializar();  
-	    } catch (Exception e) {
-	        fail(e.getMessage());
-	    }
+	    pr.inicializar();  
+	    System.out.println("-------------");
+	}
+	@org.junit.Test
+	public void emptyParams() {
+		System.out.println("Test parametros vacios");
+		String input = 	" \n ";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		Programa pr = new Programa();
+	    pr.inicializar();  
+	    System.out.println("-------------");
+	}
+	
+	@org.junit.Test
+	public void emptyParam1() {
+		System.out.println("Test parametro 1 vacio");
+		String input = 	" \nD:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\vacio.txt";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		Programa pr = new Programa();
+	    pr.inicializar();  
+	    System.out.println("-------------");
+	}
+	
+	@org.junit.Test
+	public void emptyParam2() {
+		System.out.println("Test parametro 2 vacio");
+		String input = 	"D:\\Eclipse\\eclipse-workspace\\programaDeportivo\\src\\test\\resources\\resultado.txt\nhola ";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		Programa pr = new Programa();
+	    pr.inicializar();  
+	    System.out.println("-------------");
 	}
 }
 
